018146b5ef750be21565e8846df5aba98daaefe2
tara <tara_m_oliver@yahoo.com>
2022-08-28 01:33:58 -0400

    finish project

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import SessionReducer from './session_reducer';
 import ErrorsReducer from './errors_reducer';
+import TweetsReducer from './tweets_reducer';
 
 const RootReducer = combineReducers({
     session: SessionReducer,
-    errors: ErrorsReducer
+    errors: ErrorsReducer,
+    tweets: TweetsReducer
 });
 
 export default RootReducer;
